01fbafd9113bee873c0ba07fe602fd0f1a3bb6ab
jsteiner207 <jsteiner207@gmail.com>
2025-04-07 20:51:28 -0500

    initialize repository

---
+++ src/main/java/com/archsupport/api/ArchSupportApiApplication.java
@@ -0,0 +1,13 @@
+package com.archsupport.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchSupportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchSupportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archsupport/api/ArchSupportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archsupport.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchSupportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
