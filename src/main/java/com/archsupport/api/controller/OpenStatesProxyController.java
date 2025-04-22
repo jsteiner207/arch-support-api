6540d2db694fae5dc983e3777bcaf87e43180b50
jsteiner207 <jsteiner207@gmail.com>
2025-04-21 22:56:19 -0500

    feature-add-open-states-controller: establishes proxy controller for open states api and arch-support-ui

---
+++ src/main/java/com/archsupport/api/controller/OpenStatesProxyController.java
@@ -0,0 +1,65 @@
+package com.archsupport.api.controller;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriUtils;
+
+@RestController
+@RequestMapping("/api/openstates")
+@CrossOrigin(origins = "*")
+public class OpenStatesProxyController {
+
+    private final RestTemplate restTemplate;
+    
+    @Value("${openstates.api.key}")
+    private String apiKey;
+
+
+    public OpenStatesProxyController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    // TODO: Enable redis caching
+    @Cacheable(value = "billsCache", key = "#queryParams.toString()")
+    @GetMapping("bills")
+    public ResponseEntity<String> getLegislators(@RequestParam Map<String, String> queryParams) {
+        String url = "https://v3.openstates.org/bills?" + buildQueryString(queryParams);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-API-KEY", apiKey);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        return ResponseEntity
+                .status(response.getStatusCode())
+                .body(response.getBody());
+    }
+
+        private String buildQueryString(Map<String, String> queryParams) {
+        return queryParams.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + UriUtils.encode(entry.getValue(), StandardCharsets.UTF_8))
+                .collect(Collectors.joining("&"));
+    }
+}
